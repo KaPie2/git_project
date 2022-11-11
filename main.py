bbb92ba2ea96f9d8d34ddaa8c7fe4c8f58f04b40
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2022-11-11 18:19:50 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     print('My first git program')
     print('Всем привет!')
     print('привет еще раз')
-    print('Работаем в GitHub')
+    print('Работаем в GitHub.')
 
 if __name__ == '__main__':
     main()
